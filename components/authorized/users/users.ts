c0b503958f6cb8b4c55c07070962d8003c3263a5
Mikhail Kovalchuk <mmmffjs7438g@gmail.com>
2021-04-29 14:09:36 +0300

    change types

--- components/authorized/users/users.ts
+++ components/authorized/users/users.ts
@@ -1,23 +1,26 @@
-import {FastifyInstance, FastifyReply, FastifyRequest} from 'fastify'
+import {FastifyInstance, FastifyReply, FastifyRequest, RequestGenericInterface} from "fastify";
 import {usersSchemeGet, usersSchemePost} from './routeSchemes'
 
-interface Params {
+interface IParams {
   id: number
+}
+interface IBody {
+
 }
 
 export const users = async (server: FastifyInstance) => {
-  server.get<Params>(
-    '/users/:id',
-    usersSchemeGet,
-    async (request: FastifyRequest, reply: FastifyReply) => {
-      return 'user1' + request.params.id
-    }
+  server.get<{Params: IParams }>(
+     '/users/:id',
+     usersSchemeGet,
+     async (request, reply) => {
+        return 'user1' + request.params.id
+     }
   )
 
-  server.post(
+  server.post<{Params: IParams, Body: IBody}>(
     '/users',
     usersSchemePost,
-    async (request: FastifyRequest, reply: FastifyReply) => {
+    async (request, reply) => {
       return 'it worked!2'
     }
   )
