31d7cbf759c7534e23f5a6741cd6343bcae75c4a
mk1020 <mmmffjs7438g@gmail.com>
2021-05-14 11:43:51 +0300

    add check email

--- app/components/auth/signIn.ts
+++ app/components/auth/signIn.ts
@@ -15,17 +15,17 @@ export const signIn = async (server: FastifyInstance) => {
     '/signin',
     signInScheme,
     async (req, reply) => {
-      const {password, device = 'mobile'} = req.body;
+      const {email, password, device = 'mobile'} = req.body;
 
       const passHashSent = sha256(password, envDev.passSalt);
-      const queryRes = await server.pg.query('SELECT user_id FROM root.users WHERE password_hash = $1', [passHashSent]);
+      const queryRes = await server.pg.query('SELECT user_id FROM root.users WHERE password_hash = $1 AND email = $2', [passHashSent, email]);
       !queryRes.rows.length && reply.status(403).send('Wrong password or user is not registered');
 
       const userId = queryRes.rows[0].user_id;
       const token = randomBytes(64).toString('hex');
 
       await server.pg.query(`INSERT INTO root.users_access VALUES ($1, $2, $3, current_timestamp + INTERVAL '1 month')`, [token, userId, device]);
-      reply.status(200).send();
+      reply.send();
     }
   );
 };
